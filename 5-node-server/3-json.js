8ecc8e98e6bd778329f1a8600aa7843f62d273ec
yrkim <icho0405@gmail.com>
2022-07-28 09:07:30 +0900

    디렉토리명 변경

--- 5-node-server/3-json.js
+++ 5-node-server/3-json.js
@@ -9,8 +9,8 @@ const courses = [
 ];
 
 const server = http.createServer((req, res) => {
-  const url = req.url; // what?
-  const method = req.method; // how?, action?
+  const url = req.url; // what? 클라이언트가 무엇을 원하는지
+  const method = req.method; // how?, action? 어떤 메소드로 받아와서 어떤 액션을 받아올 것인지
   if (url === '/courses') {
     if (method === 'GET') {
       res.writeHead(200, { 'Content-Type': 'application/json' });
