6394165e709c09dd5a33edc117a5353dc9bc1215
yrkim <icho0405@gmail.com>
2022-07-29 09:18:05 +0900

    add: get의 요청과 응답에 대해서

---
+++ 11-express/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+// https://expressjs.com/en/4x/api.html
+
+app.get('/sky/:id', (req, res, next) => {
+  // console.log(req.path);
+  // console.log(req.headers);
+  console.log(req.params);
+  console.log(req.query);
+
+  res.setHeader('key', 'value');
+  // res.send('hi');
+  // res.json({ name: 'april' });
+  // res.statusCode(400);
+  res.status(201).send('created');
+});
+
+app.listen(8000);
