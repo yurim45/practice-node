495fce6314ed6b7443e52c91afebb4de440743f7
yrkim <icho0405@gmail.com>
2022-08-25 08:23:45 +0900

    Sanitization이란? 중요성에 대해

--- 8-validation/app.js
+++ 8-validation/app.js
@@ -15,7 +15,7 @@ const validate = (req, res, next) => {
 app.post(
   '/users',
   [
-    body('name').trim().isLength({ min: 2 }).withMessage('이름은 두글자 이상!'),
+    body('name').trim().isLength({ min: 2 }).withMessage('이름은 두글자 이상!'), // trim() Sanitization을 위해 추가, 공백제거. 유성성 검사 전 실행하기 위해 맨 앞에 체이닝
     body('age').isInt().withMessage('숫자를 입력해'),
     body('email').isEmail().withMessage('이메일 입력해요').normalizeEmail(),
     body('job.name').notEmpty(),
