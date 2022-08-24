4ea6fdb5cf64ca79a244ea34abbc6494a425d7cc
yrkim <icho0405@gmail.com>
2022-08-25 08:35:10 +0900

    드위터에 유효성 검사 적용해 보기 💡

--- dwitter/server/router/tweets.js
+++ dwitter/server/router/tweets.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
 import * as tweetController from '../controller/tseet/tweet.js';
+import { body } from 'express-validator';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+const validateTweet = [
+  body('text').trim().isLength({ min: 3 }).withMessage('3글자 이상 입력하세요'),
+  validate,
+];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -12,10 +18,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 // PUT /tweet/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELETE /tweet/id
 router.delete('/:id', tweetController.deleteTweet);
